e461bd7ceedf000d4b341803cb1de44c3678d843
Cuong Tran <mailtocuong@gmail.com>
2023-10-22 21:10:35 +0300

    add comment

--- logEvents.js
+++ logEvents.js
@@ -5,16 +5,18 @@ const fs = require("fs");
 const fspromise = require("fs").promises;
 const path = require("path");
 
+// Write down the message with time formatting and unique identifier
 const logEvents = async (message) => {
   const dateTime = `${format(new Date(), "dd-MM-yyyy")}`;
   const logItem = `${dateTime}\t${uuidv4()}\t${message}`;
-  console.log(logItem);
 
   try {
+    // creat the folder if it doesn't exist
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fspromise.mkdir(path.join(__dirname, "logs"));
     }
 
+    // append the log item to the event log file
     await fspromise.appendFile(
       path.join(__dirname, "logs", "eventLog.txt"),
       logItem
