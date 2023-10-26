bb0bd8dc190d00d1e74bdb8a1a017772ad8f5d4e
Cuong Tran <mailtocuong@gmail.com>
2023-10-26 15:41:27 +0300

    first version of reading file base on the request path, route possibly?

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvents = require("./logEvents");
 const EventEmitter = require("events");
 
 class Emitter extends EventEmitter { };
 
+// initialize the object 
 const myEmitter = new Emitter();
 
+const PORT = process.env.PORT || 3500;
+
+// create the server
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  let requestPath;
+
+  if (req.url === "/" || req.url === "index.html") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    
+    requestPath = path.join(__dirname, "views", "index.html");
+
+    fs.readFile(requestPath, (err, data) => {
+      if (err) throw err;
+      res.end(data);
+    });
+  } else {
+    requestPath = path.join(__dirname, req.url);
+  }
+});
+
+// config the server to listen on the specified port and host
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
+
+/*
 myEmitter.on("log", (message) => logEvents(message));
 
 setTimeout(() => {
   myEmitter.emit("log", "Log event emitted");
 }, 2000);
+*/
