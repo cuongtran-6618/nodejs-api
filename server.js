502e24b19f5002e350f3b446b6002ee5beb3fa29
Cuong Tran <mailtocuong@gmail.com>
2023-11-07 23:05:19 +0200

    fix when it is undefined

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const whiteList = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) {
+    if (whiteList.indexOf(origin) !== -1  || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by cors'));
