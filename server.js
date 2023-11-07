742c1cc017ddb34215957a193aa12b2858af2842
Cuong Tran <mailtocuong@gmail.com>
2023-11-07 22:56:45 +0200

    add cors and whitelist

--- server.js
+++ server.js
@@ -2,9 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logEvents");
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+
+const whiteList = [
+  'http://www.fixably.com', 
+  'http://127.0.0.1:550', 
+  'http:localhost'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by cors'));
+    }
+  }, 
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // buildt-in middleware to handle urlendcode data (form data)
 app.use(express.urlencoded({ extended: false}));
