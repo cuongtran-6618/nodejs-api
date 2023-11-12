694932ea79c0063ce39be4c3e22109946a18d1dc
Cuong Tran <mailtocuong@gmail.com>
2023-11-12 17:03:06 +0200

    format file

--- server.js
+++ server.js
@@ -2,49 +2,49 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logEvents");
-const cors = require('cors');
+const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-
 const whiteList = [
-  'http://www.fixably.com', 
-  'http://127.0.0.1:550', 
-  'http:localhost'
+  "http://www.fixably.com",
+  "http://127.0.0.1:550",
+  "http:localhost",
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1  || !origin) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by cors'));
+      callback(new Error("Not allowed by cors"));
     }
-  }, 
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // buildt-in middleware to handle urlendcode data (form data)
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // buildt-in middleware to handle json
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
+// routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,'views', 'index.html'));
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname,'views', 'new-page.html'));
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
 app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => {
